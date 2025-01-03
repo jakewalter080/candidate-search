0a319988cbea541ae91412b800d7c7ecd8081f6f
Jake Walter <jakewalter080@gmail.com>
2025-01-03 16:15:00 -0600

    add link nav

--- Develop 9/src/components/Nav.tsx
+++ Develop 9/src/components/Nav.tsx
@@ -1,15 +1,21 @@
 const Nav = () => {
   // TODO: Add necessary code to display the navigation bar and link between the pages
-  import React from 'react';
+import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
 
 const Nav: React.FC = () => {
   const location = useLocation();
   
-  
-  
   return (
-    <div>Nav</div>
+    <nav className="bg-gray-800 p-4">
+    <div className="max-w-3xl mx-auto flex space-x-4">
+      <Link 
+        to="/" 
+        className={`text-white px-3 py-2 rounded-md ${
+          location.pathname === '/' ? 'bg-gray-900' : 'hover:bg-gray-700'
+        }`}
+      >
+      
   )
 };
 
