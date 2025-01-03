154260a985f5c3c954b675f17b876d07f3a77b24
Jake Walter <jakewalter080@gmail.com>
2025-01-03 16:06:34 -0600

    add nav imports

---
+++ Develop 9/src/components/Nav.tsx
@@ -0,0 +1,16 @@
+const Nav = () => {
+  // TODO: Add necessary code to display the navigation bar and link between the pages
+  import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
+
+const Nav: React.FC = () => {
+  const location = useLocation();
+  
+  
+  
+  return (
+    <div>Nav</div>
+  )
+};
+
+export default Nav;
